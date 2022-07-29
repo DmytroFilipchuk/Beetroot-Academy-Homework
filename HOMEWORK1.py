505ba4b765f9107ac1e069e739b0e267af3c1798
DmytroFilipchuk <110261209+DmytroFilipchuk@users.noreply.github.com>
2022-07-29 23:54:59 +0300

    Add files via upload

---
+++ HOMEWORK1.py
@@ -0,0 +1,7 @@
+#Task2
+#Hey_my_name_is_Dima!
+print("Hey","my","name","is","Dima",sep='_',end='!')
+#Task3
+print("\n#########\n#\t\t#\n#\t\t#\n#\t\t#\n#\t\t#\n#########")
+print("\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#")
+
