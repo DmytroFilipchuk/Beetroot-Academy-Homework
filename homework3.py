9bd2c7a706c59ceb6f8bada330a8b1368ecaf828
DmytroFilipchuk <110261209+DmytroFilipchuk@users.noreply.github.com>
2022-08-02 18:06:40 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -3,7 +3,7 @@ Task 1
 
 The greeting program.
 
-Make a program that has your name and the current day of the week
+Make a program that has your name and the current day of the week 
 stored as separate variables and then prints a message like this:
 
  "Good day <name>! <day> is a perfect day to learn some python."
@@ -78,4 +78,30 @@ exponent = {exponent}
 modulus = {modulus}
 floor division = {floor_division}
 """)
+
+"""
+Task 1*
+На форматування. Зробіть форматування так щоб отримати ось таке
+"000012 Василий 110110 32.10"
+Заповніть ___ , майте на увазі 110110 - це бінарний формат.
+print("____________________".format(12, "Василий", 54, 32.1))
+"""
+
+print("0000{0}{1}{2:b}{3:.2f}".format(12, "Василий", 54, 32.1))
+
+"""
+Task 2*
+Спробуйте "зібрати" зі слова "Корован" слово "ворона". 
+Використайте слайсінг та вашу фантазію. 
+s1 = "Корован"
+print(s1[4]+s1[1]+s1[2] +...  <-   можлиіий але не самий цікавий варіант.
+"""
+
+word = "Корован"
+word_reversed = word[::-1]
+part_1 = word_reversed[2:4]
+part_2 = word[2:4]
+part_3 = word_reversed[0:2]
+new_word = part_1 + part_2 + part_3
+print(new_word.capitalize())
 
