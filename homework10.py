a8ee606716d5ac188bf41c41f4cf04b9bac09d5e
DmytroFilipchuk <110261209+DmytroFilipchuk@users.noreply.github.com>
2022-08-17 21:28:05 +0300

    Update homework10.py

--- homework10.py
+++ homework10.py
@@ -47,3 +47,73 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    
+ TASKS WITH STAR
+
+"""
+Task1
+Додати у функцію з 3-го завдання уроку 8 (A simple calculator),
+обробку операції ділення. Врахувати можливий 0 у вхідних аргументах.
+"""
+from functools import reduce
+from operator import mul,sub,truediv
+
+
+def task1():
+    def make_operation(operator,*args):
+        if operator == "+":
+            numbers = sum(args)
+        if operator == "-":
+            numbers = reduce(sub,args)
+        if operator == "*":
+            numbers = reduce(mul,args)
+        if operator == "/":
+            try:
+                numbers = reduce(truediv, args)
+            except ZeroDivisionError:
+                print("You cannot divide by zero")
+                exit()
+        return numbers
+    print(make_operation("/",2,4,6,0))
+
+"""
+Task2
+Написати програму, яка на вхід приймає дату у форматі "1 January 22". 
+Програма має вивести в консоль дати всіх понеділків в межах даного місяця.
+
+"""
+def task2():
+
+    from datetime import datetime
+    from dateutil import rrule
+    import calendar
+
+    while True:
+        try:
+            user_input = input("Enter a date in a following format '1 January 22': ")
+            start_date = datetime.strptime(user_input, '%d %B %y').date()
+        except ValueError:
+            print("Wrong input")
+            continue
+        else:
+            break
+
+
+    last_day_of_mth= (calendar.monthrange(start_date.year,start_date.month)[1])
+    end_date = datetime.strptime(f"{last_day_of_mth} {start_date.month} {start_date.year}", '%d %m %Y').date()
+    all_mondays = []
+
+    for date in rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date):
+        if date.strftime("%A") == "Monday":
+            all_mondays.append(date.strftime('%d %B %y'))
+
+    print(f"\nThese are all mondays left in {start_date.strftime('%B %Y')}: \n{all_mondays}")
+
+
+def main():
+    task1()
+    #task2()
+
+if __name__ == '__main__':
+    main()
